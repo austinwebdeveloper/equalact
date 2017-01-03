06066e7fe77ec6db4d10bf23f11742f612f845fe
bala <bala@192.168.0.107>
2017-01-03 10:44:38 +0530

    Fourth Commit 3/1/17

--- wp-content/themes/justice/partials/header-standard.php
+++
@@ -1,7 +0,0 @@
-<?php get_template_part( 'partials/header' , 'logo' ); ?>
-			
-<div class="navbar-collapse tpath-mainnavbar-collapse collapse tpath-header-main-bar">
-	<ul class="nav navbar-nav tpath-main-bar">
-		<li class="header-top-left"><?php justice_header_content_area( 'main-navigation' ); ?></li>
-	</ul>
-</div>
